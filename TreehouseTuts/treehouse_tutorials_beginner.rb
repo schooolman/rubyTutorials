3015fb36892b1b7ce87c2f3dc5542547fa06acc5
Jake Schoolmeesters <j.schoolmee@gmail.com>
2017-02-14 22:35:09 -0600

    putting in the code from earlier today.

--- TreehouseTuts/treehouse_tutorials_beginner.rb
+++ TreehouseTuts/treehouse_tutorials_beginner.rb
@@ -1,1 +1,9 @@
 # This is the code from some of the treehouse tutorials that I have been doing on ruby
+
+print "please enter your name: "
+name = gets
+#gets means get string
+
+#puts "Hello " + name + "!"
+puts "Hello #{name}!"
+#puts name
