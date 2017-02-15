ac79916e67321e888ab0f4828761953e299bed7c
Jake Schoolmeesters <j.schoolmee@gmail.com>
2017-02-14 22:45:00 -0600

    moving on to irb

--- TreehouseTuts/treehouse_tutorials_beginner.rb
+++ TreehouseTuts/treehouse_tutorials_beginner.rb
@@ -7,3 +7,48 @@ name = gets
 #puts "Hello " + name + "!"
 puts "Hello #{name}!"
 #puts name
+
+#print "please enter your name: "
+#name = gets
+#gets means get string
+
+#puts "Hello " + name + "!"
+#puts "Hello #{name}!"
+#puts name
+
+#double qoutes are needed in ruby to inerpolate the variables
+#name = 'jake'
+#puts %Q(Hello #{name}!)
+#
+#string = "Hello
+#My name is Jason
+#Workspaces is fun!
+#"
+#
+#puts string
+
+
+# This is called a here-doc, the word "STRING" doesn't matter and could be anything.
+
+name = "Jake"
+string = <<-STRING
+Hello My name is #{name}
+Workspaces are alright
+STRING
+
+puts string
+
+
+#New Line character is represented with \n
+#There are many special characters that can be used
+
+#name = "Jake"
+#string = "Hello\nMy name is #{name}\nWorkspaces is fun!"
+#puts string
+
+#\t is a tab
+#\s is a space character, but is not often used.
+
+name = "Jake"
+string = "Hello\n\tMy name is #{name}\n\tWorkspaces is fun!"
+puts string
