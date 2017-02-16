fbb66599d19e785d6b4a7662bd1869a46a38a98d
Jake Schoolmeesters <j.schoolmee@gmail.com>
2017-02-15 21:32:02 -0600

    moving on to ruby methods. real similar to javascript functions

---
+++ TreehouseTuts/rubyMethods.rb
@@ -0,0 +1,13 @@
+#New Ruby workspace
+#learning about methods
+
+# Print out that we're adding two numbers together.
+# And then return the sum of the two numbers
+def add(a, b)
+  puts "adding #{a} and #{b}:"
+  return a + b
+end
+
+puts add(2, 8)
+puts add(5, 10)
+puts add(299, 3049)
