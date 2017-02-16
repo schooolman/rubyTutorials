a3a9a924098b79aacfaafb99b71556d7188903bb
Jake Schoolmeesters <j.schoolmee@gmail.com>
2017-02-15 21:47:07 -0600

    last tutorial of the night

--- TreehouseTuts/rubyMethods.rb
+++ TreehouseTuts/rubyMethods.rb
@@ -11,3 +11,8 @@ end
 puts add(2, 8)
 puts add(5, 10)
 puts add(299, 3049)
+
+# This was a stupid question that tricked me with a fucking "." one of the most frustrating things in writing code. Fucking periods.
+def modulus(a, b)
+  return "If you divide #{a} and #{b} you will get the remainer #{a%b}."
+end
