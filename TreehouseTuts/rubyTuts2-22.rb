d84b9bfee5ba797c71faca07a80da13ee7450411
Jake Schoolmeesters <j.schoolmee@gmail.com>
2017-02-23 22:11:12 -0600

    intro to ruby operators

---
+++ TreehouseTuts/rubyTuts2-22.rb
@@ -0,0 +1,16 @@
+# We're in the future!
+# Use the operator to increase the year varaible by 1
+year = 3000
+year += 1
+
+# comparison opperators -- launch irb
+a = 11
+b = 10
+a == b
+# this is an operator variable -- these will result in false
+
+name = "jake"
+site = "internet"
+#  strings are case sensitive
+"jake" != "Jake"
+#  This returns true due to the inequality operator
