0c07b8d6085fe24d42a587f5a278976a4bdcd2f9
Jake Schoolmeesters <j.schoolmee@gmail.com>
2017-02-27 19:44:01 -0600

    just finished the ruby operators course.

---
+++ TreehouseTuts/tutorialsContinued.rb
@@ -0,0 +1,39 @@
+# Working with the greater and less than operators
+
+car1 = 300
+
+car2 = 500
+
+# This will return a true boolean in irb
+car1 < car2
+
+# This will return a false variable
+car2 < car1
+
+# This retunrs true
+"a" < "b"
+
+# This returns false
+"a" > "b"
+
+# This returns true
+"treehouse" > "TREEHOUSE"
+
+# This also returns true
+# This is due to the end value of the character.
+"a" > "A"
+
+# calling this method to return the numerical value of a string
+"a".ord
+=> 97
+
+"A".ord
+=> 65
+
+#The .ord method returns the integer ordinal. Which has to do with the ASCII character code
+
+"jake".length
+=> 4
+
+"jake".length < "treehouse".length
+=> true
