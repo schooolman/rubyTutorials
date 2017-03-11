b3047a316671652789ffa1eecc5133cb9bc99835
Jake Schoolmeesters <j.schoolmee@gmail.com>
2017-03-11 14:50:13 -0600

    putting it all toether

--- TreehouseTuts/newRuby.rb
+++ TreehouseTuts/newRuby.rb
@@ -10,3 +10,38 @@ end
 if (car1_speed > 40) && !(car1_speed > car2_speed)
   puts "car1 is going over 40 but not quite as fast as car 2"
 end
+
+# Ruby interperts math and operations the same way as standard math PERMDAS
+# Parens will create importance
+
+# very basic ruby program below
+def get_name()
+  print "Enter your name: "
+  return gets.chomp
+end
+
+def greet(name)
+  puts "hi #{name}!"
+  if (name == "jake")
+    puts "what an awesome fucking name"
+  end
+end
+
+def get_number()
+  print "what number would you like to test?"
+  return gets.chomp.to_i
+end
+
+def divisible_by_3(number)
+  return (number % 3 == 0)
+end
+
+name = get_name()
+greet(name)
+number = get_number()
+
+if divisible_by_3(number)
+  puts "your number is divisible by 3"
+else
+  puts "your number is not divisible by 3"
+end
